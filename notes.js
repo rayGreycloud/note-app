1e81ef2fa470551795416f540e62da63e5a30266
rayGreycloud <rayGreycloud@gmail.com>
2016-10-07 13:21:38 -0700

    Add yargs

--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log("Notes.js activated.");
 
-module.exports.addNote = () => {
-  console.log('addNote called');
-  return 'New note';
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+  console.log('Listing all notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
