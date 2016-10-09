f07d08396716ab205deed1c3adf6cfca5e2c70f6
rayGreycloud <rayGreycloud@gmail.com>
2016-10-09 15:13:35 -0700

    Refactor and add logNote function

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
   if (note) {
     console.log('Note created.');
-    console.log('---');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    notes.logNote(note);
   } else {
     console.log("Note title already in use");
   }
@@ -29,9 +27,7 @@ if (command === 'add') {
   var note = notes.getNote(argv.title);
   if (note) {
     console.log('Note found');
-    console.log('---');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    notes.logNote(note);
   } else {
     console.log("Note not found");
   }
